fa4d8f9818a7572e69cd92f0721c9fc87fa42b03
H-Kitaura <110322542+H-Kitaura@users.noreply.github.com>
2022-10-21 23:18:27 +0900

    レイアウト編集

--- app/javascript/nav.js
+++ app/javascript/nav.js
@@ -2,16 +2,25 @@ function pullDown(){
   const pullDownButton = document.getElementById("nav")
   const pullDownParents = document.getElementById("pull-down")
 
-  pullDownButton.addEventListener('click', function() {
-    if (pullDownParents.getAttribute("style") == "display:block;") {
-      // pullDownParentsにdisplay:block;が付与されている場合（つまり表示されている時）実行される
-      pullDownParents.setAttribute("style", "display:none;")
-    
-    } else {
-      // pullDownParentsにdisplay:block;が付与されていない場合（つまり非表示の時）実行される
-      pullDownParents.setAttribute("style", "display:block;")
-    }
-   })
-  }
-   window.addEventListener('load', pullDown)
+    pullDownButton.addEventListener('mouseover', function(){
+      this.setAttribute("style", "background-color:#FFF;")
+    })
+
+    pullDownButton.addEventListener('mouseout', function(){
+      this.removeAttribute("style", "background-color:#FFF;")
+    })
+
+    pullDownButton.addEventListener('click', function() {
+      if (pullDownParents.getAttribute("style") == "display:block;") {
+        // pullDownParentsにdisplay:block;が付与されている場合（つまり表示されている時）実行される
+        pullDownParents.setAttribute("style", "display:none;")
+      
+      } else {
+        // pullDownParentsにdisplay:block;が付与されていない場合（つまり非表示の時）実行される
+        pullDownParents.setAttribute("style", "display:block;")
+      }
+    })
+  } 
+
+ window.addEventListener('load', pullDown)
   
