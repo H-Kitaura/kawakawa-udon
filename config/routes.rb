029e08152c63d686a33730dcd4130b4274991473
H-Kitaura <110322542+H-Kitaura@users.noreply.github.com>
2022-10-26 17:02:57 +0900

    レスポンシブ化

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to:"pages#index"
    resources :pages, only: [:index]
-   get'form',to: 'pages#form'
-   get'attention',to: 'pages#attention'
    resources :menus
    get 'index2', to: 'menus#index2'
    get 'index3', to: 'menus#index3'
